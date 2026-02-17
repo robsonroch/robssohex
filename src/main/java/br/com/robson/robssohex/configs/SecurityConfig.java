2eefaceea9b85628ee994e774c9802a9d18f37a1
robsonroch <robson.proxycode@gmail.com>
2026-02-17 07:10:50 -0300

    feat: acesso back-end via proxy nginix

--- src/main/java/br/com/robson/robssohex/configs/SecurityConfig.java
+++ src/main/java/br/com/robson/robssohex/configs/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
